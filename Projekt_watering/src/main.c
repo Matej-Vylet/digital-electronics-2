6c402c9d08913b7e4ce69e26c36fbaa5a584dfac
Matej-Vylet <240715@vut.cz>
2023-11-29 15:03:47 +0100

    projekt

--- Projekt_watering/src/main.c
+++ Projekt_watering/src/main.c
@@ -28,6 +28,7 @@
 #include <stdlib.h>         // C library. Needed for number conversions
 #include <oled.h>
 #include <gpio.h>
+#include <stdio.h>
 
 /* Global variables --------------------------------------------------*/
 // Declaration of "dht12" variable with structure "DHT_values_structure"
@@ -41,6 +42,7 @@ struct DHT_values_structure {
 } dht12;
 uint16_t dry_max = 900;
 uint16_t wet_max = 670;
+uint16_t value = 0;
 
 // Flag for printing new data from sensor
 volatile uint8_t new_sensor_data = 0;
@@ -52,8 +54,8 @@ volatile uint8_t new_sensor_data = 0;
 #define SENSOR_TEMP_MEM 2
 #define SENSOR_CHECKSUM 4
 #define AN PC0
-#define RELE PB2
-uint16_t value = 0;
+#define RELE PD2
+
 
 
 /* Function definitions ----------------------------------------------*/
@@ -64,12 +66,10 @@ uint16_t value = 0;
 **********************************************************************/
 int main(void)
 {
-    /* GPIO_mode_output(&DDRB, AN); */
-    GPIO_mode_output(&DDRB, RELE);
+    GPIO_mode_output(&DDRD, RELE);
     
     ADMUX |= (1<<REFS0);
     ADMUX &= ~(1<<REFS1);
-
     ADMUX &= ~((1<<MUX3) | (1<<MUX2) | (1<<MUX1) | (1<<MUX0) );
 
     // Enable ADC module
@@ -79,34 +79,29 @@ int main(void)
     // Set clock prescaler to 128
     ADCSRA |= (1<<ADPS2 | 1<<ADPS1 | 1<<ADPS0);
 
-    TCCR1A &= ~((1 << COM1A0) | (1 << COM1B0));
-    TCCR1A |= (1 << WGM11) | (1 << COM1A1) | (1 << COM1B1);
-    TCCR1B |= (1 << WGM13);
-
     ICR1 = 20000;
     OCR1A = AN;
-    
-    /* TCCR1B |= (1 << CS11) | (1 << CS10);  */
-  uint8_t radek =0;
-  oled_init(OLED_DISP_ON);
-    oled_clrscr();
 
+    oled_init(OLED_DISP_ON);
+    oled_clrscr();
 
     // Copy buffer to display RAM
     oled_display();
     char string[2];  // String for converting numbers by itoa()
-
+    
     // TWI
     twi_init();
-
+    
     // UART
     uart_init(UART_BAUD_SELECT(115200, F_CPU));
 
-    sei();  // Needed for UART
-
     // Test if sensor is ready
     if (twi_test_address(SENSOR_ADR) == 0)
+      {
         uart_puts("I2C sensor detected\r\n");
+        uart_puts("Teperature\t");
+        uart_puts("Humidity\r\n");
+      }
     else {
         uart_puts("[ERROR] I2C device not detected\r\n");
         while (1);
@@ -116,17 +111,17 @@ int main(void)
     TIM1_OVF_1SEC
     TIM1_OVF_ENABLE
 
-    sei();
+    sei(); // Needed for UART
 
     // Infinite loop
     while (1) {
-      
-        if (new_sensor_data == 1) {
-
+        if (new_sensor_data == 1)
+          {
             oled_gotoxy(0,0);
             oled_charMode(NORMALSIZE);
 
             itoa(dht12.temp_int, string, 10);
+            oled_puts("Temperature: ");
             oled_puts(string);
             uart_puts(string);
             uart_puts(".");
@@ -134,58 +129,33 @@ int main(void)
 
             itoa(dht12.temp_dec, string, 10);
             uart_puts(string);
-            uart_puts(" °C\t");
+            uart_puts(",");
             oled_puts(string);
-            oled_puts("C ");
-
-            itoa(dht12.hum_int, string, 10);
-            uart_puts(string);
-            uart_puts(".");
-            oled_puts(string);
-            oled_puts(".");
-
-            itoa(dht12.hum_dec, string, 10);
-            uart_puts(string);
-            uart_puts(" % ");
-            oled_puts(string);
-            oled_puts(" % ");
+            oled_puts(" °C \r\n\n");
 
             itoa(dht12.cap_hum,string,10);
+            oled_puts("Humidity: ");
             uart_puts(string);
-            uart_puts(" % \r\n");
+            uart_puts("\r\n");
             oled_puts(string);
-            oled_puts(" % \r\n");
+            oled_puts(" % \r\n\n");
 
-
-
-            oled_display();
-
-            if( dht12.cap_hum>870) //je sucho
+            if( dht12.cap_hum<45) //je sucho
               {
-                 GPIO_write_low(&PORTB, RELE);
-                 // PORTB &= ~(1<<RELE);
+                GPIO_write_low(&PORTD, RELE);
+                oled_puts("Watering: ON ");                 
               }
-              if(dht12.cap_hum<700)
-             {
-                GPIO_write_high(&PORTB, RELE);
-               // PORTB |= (1<<RELE);
-             }
-            /* if(radek<8)
-            {
-              radek++;
-            }
-            else
-            {
-              oled_clrscr();
-              radek=0;
-              
-            } */
+            if(dht12.cap_hum>85)
+              {
+                GPIO_write_high(&PORTD, RELE);
+                oled_puts("Watering: OFF");  
+              }
 
             // Do not print it again and wait for the new data
             new_sensor_data = 0;
-        }
-    }
-
+            oled_display();
+          }
+  }
     // Will never reach this
     return 0;
 }
@@ -200,10 +170,12 @@ ISR(TIMER1_OVF_vect)
 {
     // Test ACK from sensor
     twi_start();
-    if (twi_write((SENSOR_ADR<<1) | TWI_WRITE) == 0) {
+    if (twi_write((SENSOR_ADR<<1) | TWI_WRITE) == 0) 
+      {
         // Set internal memory location
         twi_write(0x00);
         twi_stop();
+
         // Read data from internal memory
         twi_start();
         twi_write((SENSOR_ADR<<1) | TWI_READ);
@@ -212,12 +184,10 @@ ISR(TIMER1_OVF_vect)
         dht12.temp_int = twi_read(TWI_ACK);
         dht12.temp_dec = twi_read(TWI_NACK);
         twi_stop();
+
         ADCSRA |= (1<<ADSC);
-
-
-
         new_sensor_data = 1;
-    }
+      }
     twi_stop();
 }
 
@@ -230,11 +200,6 @@ ISR(ADC_vect)
     // Read converted value
     // Note that, register pair ADCH and ADCL can be read as a 16-bit value ADC
     value = ADC;
-    itoa(value, string, 10);
-    dht12.cap_hum = value;
-
-   
-    
-    
+    dht12.cap_hum =(950-value)*100/300;  
 }
 
